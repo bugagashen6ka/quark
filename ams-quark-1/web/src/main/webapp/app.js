f0b74ef3ff6ca8955e924af1a29d8b1032a734a5
alexo_o <alexo_o@ukr.net>
2014-06-05 21:43:16 +0200

    Fixed Add Appointment form

--- ams-quark-1/web/src/main/webapp/app.js
+++ ams-quark-1/web/src/main/webapp/app.js
@@ -1,7 +1,14 @@
 $(document).ready( function () {
 	$(document).foundation();
+	//console.log("test");
 	
 
-	
+	window["fix"] = function () {
+		$('.ui-selectonemenu-trigger').width(17);
+		$('.ui-selectcheckboxmenu-trigger').width(17);
+		
+    };
+
+
 	
 });
