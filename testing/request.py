9e310376f20d4c0c4cdc4b7c7981861b3f1e5638
alexo_o <alexo_o@ukr.net>
2014-06-09 17:38:05 +0200

    Echo test with basic auth

--- testing/request.py
+++ testing/request.py
@@ -1,4 +1,5 @@
 import requests
+from requests.auth import HTTPBasicAuth
 
 class Request(object):
 	"""docstring for Request"""
@@ -7,16 +8,35 @@ class Request(object):
 	def __init__(self, url):
 		self.url = url
 		self.payload = {}
+		self.auth = False
 
 
 	def send(self):
-		if self.method == 'GET':
-			r = requests.get(self.url, params=self.payload)
-		elif self.method == 'POST':
-			r = requests.post(self.url, data=self.payload)
-		else:
-			return "Method is not defined"
-		return r.text
+		try:
+			if self.method == 'GET':
+				if self.auth:
+					r = requests.get(self.url, params=self.payload, auth=HTTPBasicAuth(self.user, self.password))
+				else:
+					r = requests.get(self.url, params=self.payload)
+			elif self.method == 'POST':
+				if self.auth:
+					r = requests.post(self.url, data=self.payload, auth=HTTPBasicAuth(self.user, self.password))
+				else:
+					r = requests.post(self.url, data=self.payload)
+			else:
+				return "Method is not defined"
+			self.lastResponce = r.text
+		except Exception as ex:
+			self.lastResponce = ex.__str__
+		return self.lastResponce
+
+	def basicAuth(self, user, password):
+		self.user = user
+		self.password = password
+		self.auth = True
+
+	def getLastResponce(self):
+		return self.lastResponce
 
 	def addParameter(self, param):
 		self.payload.update(param)
