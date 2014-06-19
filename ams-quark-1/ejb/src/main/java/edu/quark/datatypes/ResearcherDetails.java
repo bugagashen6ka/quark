b45a8350058de9b823beeb03cba9ff3cd7ba341c
Maksym Planeta <mcsim.planeta@gmail.com>
2014-06-19 13:22:27 +0200

    Fix some bugs in REST services

--- ams-quark-1/ejb/src/main/java/edu/quark/datatypes/ResearcherDetails.java
+++ ams-quark-1/ejb/src/main/java/edu/quark/datatypes/ResearcherDetails.java
@@ -15,6 +15,7 @@ public class ResearcherDetails {
 		this.title = title;
 		this.phoneNbr = phoneNbr;
 	}
+
 	public ResearcherDetails(Researcher r) {
 		super();
 		this.rId = r.getRid();
@@ -24,7 +25,7 @@ public class ResearcherDetails {
 		this.title = r.getTitle();
 		this.phoneNbr = r.getPhoneNumber();
 	}
-	
+
 	private BigInteger rId;
 	private String emailAddress;
 	private String firstName;
@@ -40,19 +41,27 @@ public class ResearcherDetails {
 	public BigInteger getrId() {
 		return rId;
 	}
+
 	public String getEmailAddress() {
 		return emailAddress;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public String getPhoneNbr() {
 		return phoneNbr;
+	}
+
+	public ResearcherDetails() {
 	}
 }
